3eabb546d2cfddad36c382332c1a59379e4a4132
Quang Duy <duy1233d@gmail.com>
2026-09-03 13:52:19 +0700

    Update requirements

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,17 +74,21 @@ WSGI_APPLICATION = 'hello_world.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'MyDatabase',
-        'USER': 'postgres',
-        'PASSWORD': '0904',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:              
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'MyDatabase',
+            'USER': 'postgres',
+            'PASSWORD': '0904',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
