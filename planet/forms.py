ce4a5c060ae6328aaa1cd69787cdab241cd6fcee
Quang Duy <duy1233d@gmail.com>
2026-09-07 14:54:20 +0700

    Add audio field to Post model and form

--- planet/forms.py
+++ planet/forms.py
@@ -35,9 +35,9 @@ class RegistrationForm(forms.Form):
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'description', 'content', 'image')
+        fields = ('title', 'description', 'content', 'image', 'audio')
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title of the Blog'}),
             'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Copy the title with no space and a hyphen in between'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Content of the Blog'}),
-        }        
+        }
--- planet/models.py
+++ planet/models.py
@@ -3,23 +3,26 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-# Create your models here.
 class Planet(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
-  phone = models.IntegerField(null=True)
-  dob = models.DateField(null=True)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True, blank=True)
+    dob = models.DateField(null=True, blank=True)
 
-  def __str__(self):
-    return f"{self.firstname} {self.lastname}"
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     content = models.TextField()
-    image = models.ImageField(null=True)
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    audio = models.FileField(upload_to='music/', null=True, blank=True)  # Thêm audio tại đây
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
@@ -29,6 +32,7 @@ class Post1(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='posts/', null=True, blank=True)
-    audio = models.FileField(upload_to='madia/music', null=True)
+    audio = models.FileField(upload_to='music/', null=True, blank=True)
+
     def __str__(self):
         return self.title
